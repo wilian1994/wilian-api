17429960253d860d64f347842f375853b31c9820
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-01-22 22:17:41 -0200

    Finalizando a aula: colecao vazio o que retornar

--- src/main/java/com/example/wilian/api/resources/CategoriaResource.java
+++ src/main/java/com/example/wilian/api/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.example.wilian.api.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,14 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+//	@GetMapping
+//	public ResponseEntity<?> listar(){
+//		List<Categoria> categorias = categoriaRepository.findAll();
+//		return categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+//	}
+	
 	@GetMapping
 	public List<Categoria> listar(){
-		System.out.println(categoriaRepository.findAll());
 		return categoriaRepository.findAll();
 	}
 
